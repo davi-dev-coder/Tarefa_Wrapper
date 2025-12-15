de0bfab4ae62677bc1544c616a5879b5d71d06c9
davi-dev-coder <daviresidentevil17@gmail.com>
2025-12-15 08:54:50 -0300

    Add files via upload

---
+++ Desafios.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.desafios;
+import java.util.Scanner;
+
+//calcula a média de 4 notas
+public class Desafios {
+    public static void main (String[] args){
+        Scanner teclado = new Scanner(System.in);
+
+        int num = teclado.nextInt();
+        Integer num2 = num;
+        
+    }
+}
